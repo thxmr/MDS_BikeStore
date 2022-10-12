4233ab94a8b1cb04b99a5e6386a7fa119588246a
[Ayoub] <[ayoublzr93@gmail.com]>
2022-10-12 11:16:07 +0200

    enhance/loginform and register

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,5 +1,22 @@
-function LoginForm(){
-    return <>
-    </>
+
+import React from 'react'
+import Login from './Index/Login';
+import Signup from './Index/Signup';
+// import Error from './Index/ErrorPage';
+
+
+
+function LoginForm() {
+    
+  return (
+    <div className="LoginApp" >
+        <Login/>
+        <Signup/>
+
+      
+    </div>
+  );
 }
+
 export default LoginForm;
+
