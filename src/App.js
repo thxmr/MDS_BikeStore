a88d50680acb9e9dac25476332d146a6f4da7767
thxmr <trohymas@gmail.com>
2022-10-12 11:28:49 +0200

    enhance/add-router for Home and Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 import Home from './Pages/Home';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>    
 );
 }
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,17 @@
+import LoginForm from '../Components/LoginForm/LoginForm'
+
+import backgroundImage from '../Components/LoginForm/img/Vélo.jpg';
+const loginStyle = {
+    'backgroundImage':{backgroundImage}
+}
 function Login(){
-    return <>
+    return( 
+    <>
+        <div id="login" style={loginStyle}>
+            <LoginForm></LoginForm>
+        </div>
+
     </>
+    );
 }
 export default Login;
