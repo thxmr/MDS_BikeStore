1db24335ecd0ca50d68d045c53272baba95d89a9
thxmr <trohymas@gmail.com>
2022-10-12 14:44:51 +0200

    enhance/add-profile-page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 
 import Home from './Pages/Home';
 import Login from './Pages/Login';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile/>}/>
       </Routes>
     </>    
 );
--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,12 +1,13 @@
 const inputStyle = {
     'marginTop':'20px',
+    'width':'fit-content',
 }
 
 function Input(props){
     return (
         <>
         <div style={inputStyle}>
-            <label htmlFor={props.name}>{props.label}</label>
+            <label htmlFor={props.name}>{props.label}</label><br/>
             <input type={props.type} name={props.name} placeholder={props.placeholder}></input>
         </div>
         </>
