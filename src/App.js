0eaa1aabd15566285c195d4f3e99f8ae23d6f936
thxmr <trohymas@gmail.com>
2022-10-12 15:16:06 +0200

    enhance/added connexion between home and product

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Profile from './Pages/Profile';
 import Signup from './Components/Signup/Signup';
+import BikeStoreDetails from './Components/BikeStoreDetails/BikeStoreDetails';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile/>}/>
         <Route path="/signup" element={<Signup/>}/>
+        <Route path="/product" element={<BikeStoreDetails/>}/>
       </Routes>
     </>    
 );
